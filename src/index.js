a6e6c6fa89fe093070c4e7115119027cc0f182ee
Ville Jänkälä <ville.jankala@gmail.com>
2018-09-07 22:00:51 +0300

    Server basics

---
+++ src/index.js
@@ -0,0 +1,29 @@
+var server = require("http").createServer();
+var io = require("socket.io")(server);
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+
+  socket.on("createvertex", (v) => {
+    socket.broadcast.emit('createvertex', v);
+  });
+
+  socket.on("createpolygon", (p) => {
+    socket.broadcast.emit("createpolygon", p);
+  });
+
+  socket.on("deletevertex", (v) => {
+    socket.broadcast.emit("deletevertex", v);
+  });
+
+  socket.on("updatevertex", (v) => {
+    socket.broadcast.emit("updatevertex", v);
+  });
+
+  socket.on("deletepolygon", (p) => {
+    socket.broadcast.emit("deletepolygon", p);
+  });
+
+});
+
+server.listen(3123);
