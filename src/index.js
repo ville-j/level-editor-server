65b7af207e4a1db4bc3948cec3a766a8602a5502
Ville Jänkälä <ville.jankala@gmail.com>
2018-09-25 23:05:15 +0300

    Room support

--- src/index.js
+++ src/index.js
@@ -5,37 +5,78 @@ io.on("connection", (socket) => {
   console.log("user connected");
 
   socket.on("createvertex", (v) => {
-    socket.broadcast.emit("createvertex", v);
+    Object.keys(socket.rooms).map(r => {
+      socket.broadcast.to(r).emit("createvertex", v);
+    });
   });
 
   socket.on("createpolygon", (p) => {
-    socket.broadcast.emit("createpolygon", p);
+    Object.keys(socket.rooms).map(r => {
+      socket.broadcast.to(r).emit("createpolygon", p);
+    });
   });
 
   socket.on("deletevertex", (v) => {
-    socket.broadcast.emit("deletevertex", v);
+    Object.keys(socket.rooms).map(r => {
+      socket.broadcast.to(r).emit("deletevertex", v);
+    });
   });
 
   socket.on("updatevertex", (v) => {
-    socket.broadcast.emit("updatevertex", v);
+    Object.keys(socket.rooms).map(r => {
+      socket.broadcast.to(r).emit("updatevertex", v);
+    });
   });
 
   socket.on("updateobject", (o) => {
-    socket.broadcast.emit("updateobject", o);
+    Object.keys(socket.rooms).map(r => {
+      socket.broadcast.to(r).emit("updateobject", o);
+    });
   });
 
   socket.on("deletepolygon", (p) => {
-    socket.broadcast.emit("deletepolygon", p);
+    Object.keys(socket.rooms).map(r => {
+      socket.broadcast.to(r).emit("deletepolygon", p);
+    });
   });
 
   socket.on("createobject", (o) => {
-    socket.broadcast.emit("createobject", o);
+    Object.keys(socket.rooms).map(r => {
+      socket.broadcast.to(r).emit("createobject", o);
+    });
   });
 
   socket.on("deleteobject", (o) => {
-    socket.broadcast.emit("deleteobject", o);
+    Object.keys(socket.rooms).map(r => {
+      socket.broadcast.to(r).emit("deleteobject", o);
+    });
+  });
+
+  socket.on("joinroom", (r) => {
+    const clients = getClients(r);
+    socket.join(r);
+
+    if (clients.length > 0) {
+      io.to(`${clients[0]}`).emit("requestlevel", `${socket.id}`);
+    }
+  });
+
+  socket.on("responselevel", (l) => {
+    io.to(`${l.clientId}`).emit("responselevel", l.level);
   });
 
 });
+
+function getClients(room) {
+  let clients = [];
+  if (!io.nsps['/'].adapter.rooms[room])
+    return clients;
+  else {
+    Object.keys(io.nsps['/'].adapter.rooms[room].sockets).map(c => {
+      clients.push(c);
+    });
+  }
+  return clients;
+}
 
 server.listen(3123);
